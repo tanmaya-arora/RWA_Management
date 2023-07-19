9fa84192bfd14a0b084a9fa17c5cf135684bff7d
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-07-19 18:54:59 +0530

    Password reset implementation by customizing Django templates

--- member/urls/member_urls.py
+++ member/urls/member_urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from django.contrib.auth.views import (
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    PasswordResetDoneView,
+    PasswordResetView
+)
 from member.views import member_views as views
 
 urlpatterns = [
     path('', views.get_all_members, name='rwa-members-listing'),
     path('register/', views.register_member, name='add-rwa-member'),
     path('login/', views.login_member, name='rwa-member-login'),
-    path('password_reset/', views.reset_password, name='password_reset')
+    path('password_reset/', views.reset_password, name='password_reset'),
+    path('password-reset/', PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete')
 ]
