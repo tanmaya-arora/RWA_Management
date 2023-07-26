4ec40ede5ab0b58cc49f788d757f7dfee0517b45
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-07-26 14:34:49 +0530

    Added payment model for storing fee payment details

--- member/models.py
+++ member/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
@@ -110,6 +111,13 @@ class Donation(models.Model):
 
     def __str__(self):
         return str(self.donation_id)
+
+class Payment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    payment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now=True)
+    payment_method = models.CharField(max_length=50)
 
 
 class Chat(models.Model):
