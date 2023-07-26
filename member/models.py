a14d409ada28ea79220a206a1a73eb5bae4533dd
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-07-26 13:12:04 +0530

    Fixed family model not returning object name

--- member/models.py
+++ member/models.py
@@ -77,6 +77,9 @@ class FamilyMember(models.Model):
         ('', ''), ('M', 'male'), ('F', 'female')), default='')
     relation = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
 
 class Tenant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='tenant')
