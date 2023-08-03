884766609620126d43ceb66a38bb9cb88e3e286d
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-08-03 18:15:22 +0530

    Restore changes in cart view POST API to add items in cart

--- member/views/cart_views.py
+++ member/views/cart_views.py
@@ -19,32 +19,32 @@ def get_cart_items(request):
 def add_to_cart(request):
     body = request.body
     data_str = body.decode('utf-8')
-    # data_dict = json.loads(data_str)
+    data_dict = json.loads(data_str)
 
-    print("Body in add_to_cart API ",body)
-    # for items in data_dict:
-    #     # data = json.loads(items)
-    #     print(items)
-    #     user = User.objects.filter(email=items['user']).first()
-    #     package = items.get('package')
+    #print("Body in add_to_cart API ",body)
+    for items in data_dict:
+        # data = json.loads(items)
+        print(items)
+        user = User.objects.filter(email=items['user']).first()
+        package = items.get('package')
 
-    #     try:
-    #         total_price = float(items.get('total_price'))
-    #     except:
-    #         total_price = 0.00
+        try:
+            total_price = float(items.get('total_price'))
+        except:
+            total_price = 0.00
         
-    #     # serializer = UserSerializer(user, many=False)
+        # serializer = UserSerializer(user, many=False)
 
-    #     try:
+        try:
         
-    #         cart_item = Cart.objects.create(
-    #             package = package,
-    #             user = user,
-    #             total_price = total_price
-    #         )
+            cart_item = Cart.objects.create(
+                package = package,
+                user = user,
+                total_price = total_price
+            )
 
-    #         cart_item.save()
-    #     except Exception as e:
-    #         return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            cart_item.save()
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
-    return JsonResponse({"message": data_str}, status=status.HTTP_200_OK)
+    return JsonResponse({"message": "Data saved successfully"}, status=status.HTTP_200_OK)
