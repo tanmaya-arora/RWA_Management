ed69b7ade49a352cc55c147de914f826f24225bf
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-08-07 13:22:58 +0530

    Internal Server Error fix

--- member/views/payment_views.py
+++ member/views/payment_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.forms import PaymentForm
+# from django.forms import PaymentForm
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import uuid
@@ -8,21 +8,21 @@ from django.conf import settings
 
 PAYSTACK_SECRET_KEY = settings.PAYSTACK_SECRET_KEY
 
-def payment_views(request):
-    if request.method == 'POST':
-        form = PaymentForm(request.POST)
-        if form.is_valid():
-            payment = form.save(commit=False)
-            payment.user = request.user
-            payment.save()
-            # Add any additional logic here, such as processing the payment or sending payment confirmation emails
-            return redirect('payment_success')  # Redirect to a success page after payment
-    else:
-        form = PaymentForm()
-    return render(request, 'payment_form.html', {'form': form})
+# def payment_views(request):
+#     if request.method == 'POST':
+#         form = PaymentForm(request.POST)
+#         if form.is_valid():
+#             payment = form.save(commit=False)
+#             payment.user = request.user
+#             payment.save()
+#             # Add any additional logic here, such as processing the payment or sending payment confirmation emails
+#             return redirect('payment_success')  # Redirect to a success page after payment
+#     else:
+#         form = PaymentForm()
+#     return render(request, 'payment_form.html', {'form': form})
 
-def payment_success_view(request):
-    return render(request, 'payment_success.html')
+# def payment_success_view(request):
+#     return render(request, 'payment_success.html')
 
 @api_view(['POST'])
 def initiate_payment(request):
