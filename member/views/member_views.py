fde2eeb8101eba4d1bea88e4afa149e10fbb85d7
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-09-01 15:31:22 +0530

    Fix unauthorized member is able to login

--- member/views/member_views.py
+++ member/views/member_views.py
@@ -150,6 +150,11 @@ def login_member(request):
     
     elif member != None:
         member_obj = Member.objects.get(email=useremail)
+        
+        if member_obj['is_verified'] == False:
+            message = {'error': 'Member is not verified to login to the RWA', 'is_verified': member_obj['is_verified']}
+            return Response(message, status=status.HTTP_401_UNAUTHORIZED)
+        
         token = requests.post("https://lobster-app-et3xm.ondigitalocean.app/token/", data=
                                 {
                                     'username': useremail,
