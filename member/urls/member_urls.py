63d54a0f73f7ce9a95058ae6f1fd02ba5ef9a671
ayush-0987 <ayush@vector3.digital>
2023-09-06 10:25:41 +0530

    member urls update for sending and receiving data

--- member/urls/member_urls.py
+++ member/urls/member_urls.py
@@ -8,15 +8,15 @@ from django.contrib.auth.views import (
 from member.views import member_views as views
 
 urlpatterns = [
-    path('', views.get_all_members, name='rwa-members-listing'),
-    path('<str:pk>', views.get_member, name='rwa-member-listing'),
-    path('otp/', views.generate_otp, name='send_otp'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
-    path('register/', views.register_member, name='add-rwa-member'),
-    path('login/', views.login_member, name='rwa-member-login'),
-    path('password_reset/', views.reset_password, name='password_reset'),
+    path('', views.get_all_members, name='rwa-member-listing'),
+    path('otp/', views.generate_otp, name='generate-otp'),
+    path('verify/', views.verify_jwt, name='verify-jwt'),
+    path('register/', views.register_member, name='register-member'),
+    path('login/', views.login_member, name='login-member'),
+    path('reset/', views.reset_password, name='reset-password'),
+    # path('send-email/', views.send_email_to_client, name='send-email'),
     path('password-reset/', PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password-reset-done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete')
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
