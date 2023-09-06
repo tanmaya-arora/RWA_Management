6afca12bba37e43778d3b8ab467a6880f7b81d09
ayush-0987 <ayush@vector3.digital>
2023-09-06 10:23:03 +0530

    member view update for login and password reset

--- member/views/member_views.py
+++ member/views/member_views.py
@@ -1,97 +1,135 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from member.models import Member, City, Country, Society, State, Tenant
+from member.models import Member, City, Country, Society, State
 from django.contrib.auth.models import User
-from member.serializers import MemberSerializer, UserSerializer, UserSerializerWithToken
+from member.serializers import MemberSerializer
 from django.contrib.auth.hashers import make_password
-from rest_framework import status
-import requests
-import json
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import random
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from rest_framework import status
 import json
 from rest_framework import status
+import os
 
 @api_view(['GET'])
 def get_all_members(request):
-    try:
-        member = Member.objects.all()
-        serializer = MemberSerializer(member, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-    except Exception as e:
-        message = {'error': str(e)}
-        return Response(message, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    member = Member.objects.all()
+    serializer = MemberSerializer(member, many=True)
+    return Response(serializer.data)
 
-@api_view(['GET'])
-def get_member(request, pk):
-    member = Member.objects.get(member_id = pk)
-    serializer = MemberSerializer(member, many=False)
-    message = {'Info': 'Member details fetched successfully', 'data': serializer.data}
-    return Response(message, status=status.HTTP_200_OK)
+@api_view(['POST'])
+def generate_otp(request):
+    email = request.data.get('email')
+    
+    try:
+        member = Member.objects.get(email=email)
+    except Member.DoesNotExist:
+        return Response({"error": "Member not found"}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({"error": f"An error occurred: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    otp = str(random.randint(1000, 9999))
+
+    member.otp = otp
+    member.save()
+
+    subject = "Your OTP Code"
+    message = f"Your OTP code is: {otp}"
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, from_email, recipient_list)
+
+    return Response({"message": "OTP sent successfully"}, status=status.HTTP_200_OK)
+
+            
+@api_view(['POST'])
+def verify_jwt(request):
+    otp = request.data.get('otp')
+    try:
+        refresh = RefreshToken()
+        access_token = refresh.access_token
+        user_id = access_token.payload.get('email')
+
+        refresh_token_validity = int(os.getenv("REFRESH_TOKEN_VALIDITY", 30)) 
+
+        expiry_timestamp = access_token.payload['exp']
+        current_timestamp = datetime.utcnow()
+        refresh_token_expiry_time = current_timestamp + timedelta(seconds=refresh_token_validity)
+        
+        if expiry_timestamp < refresh_token_expiry_time.timestamp():
+            return Response({"message": "Refresh token has expired"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+    except (TokenError, ValueError):
+        return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        user = Member.objects.get(otp=otp)
+
+        if user.otp == otp:
+            user.is_verified = True
+            user.save()
+            return Response({"message": "OTP verified successfully"}, status=status.HTTP_200_OK)
+
+        else:
+            return Response({"error":"OTP formatting"})
+        
+    except Member.DoesNotExist:
+        return Response({"error": "Member not found or Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def register_member(request):
-    data = request.body
-
-    # Decode the bytes into a string
-    data_str = data.decode('utf-8')
-
-    data_dict = json.loads(data_str)
-    
-    # splitlist = str(data).split('&')
-    
-    # name = splitlist[0].split('=')[1]
-    # email_raw = splitlist[1].split('=')[1]
-    # password_raw = splitlist[2].split('=')[1]
-    
-    # email = email_raw.replace('%40', '@')
+    data = request.data
   
-    if not 'dob' in data_dict:
-        data_dict['dob']=date.today()
-    
-    data_dict['hno']=random.randint(1,1000)
-    data_dict['area']=Society.objects.filter(area='Ardee City Sector 52').first()
-    data_dict['city']=City.objects.filter(city='Gurgaon').first()
-    data_dict['state']=State.objects.filter(state='Haryana').first()
-    data_dict['country']=Country.objects.filter(country='India').first()
-    
+    if not 'dob' in data:
+        data['dob'] = date.today()
+
+    data['hno'] = random.randint(1, 1000)
+    data['area'] = Society.objects.filter(area='Ardee City Sector 52').first()
+    data['city'] = City.objects.filter(city='Gurgaon').first()
+    data['state'] = State.objects.filter(state='Haryana').first()
+    data['country'] = Country.objects.filter(country='India').first()
+
     try:
-        user_obj = User.objects.create(
-            first_name=data_dict['first_name'],
-            last_name=data_dict['last_name'],
-            username=data_dict['email'],
-            email=data_dict['email'],
-            password=make_password(data_dict['password'])
-        )
-        
-        # when we register a user, we need to return the token
-        serializer = UserSerializerWithToken(user_obj, many=False)
-        
+        email = data.get('email')
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            return Response({"error": "User with this email already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            username=email,
+            email=email,
+            password=make_password(data['password'])
+        )    
+
         member = Member.objects.create(
-            fname = data_dict['first_name'],
-            lname = data_dict['last_name'],
-            gender = data_dict['gender'],
-            email = data_dict['email'],
-            phone_no = data_dict['phone'],
-            date_of_birth = data_dict['dob'],
-            res_hno = data_dict['hno'],
-            res_area = data_dict['area'],
-            res_city = data_dict['city'],
-            res_state = data_dict['state'],
-            res_country = data_dict['country'],
-            user = User.objects.filter(email=data_dict['email']).first()
+            user=user, 
+            fname=data['first_name'],
+            lname=data['last_name'],
+            gender=data['gender'],
+            email=email,
+            phone_no=data['phone'],
+            date_of_birth=data['dob'],
+            res_hno=data['hno'],
+            res_area=data['area'],
+            res_city=data['city'],
+            res_state=data['state'],
+            res_country=data['country'],
         )
 
-        slz = MemberSerializer(member, many=False)
-        message = {'User': serializer.data, 'Member': slz.data}
-
-        return Response(message, status=status.HTTP_200_OK)
+        response_data = {
+            "message": "User registered successfully. Please check your email for verification."
+        }
+        
+        return Response(response_data, status=status.HTTP_201_CREATED)
+            
 
     except Exception as e:
         message = {'error': str(e)}
@@ -99,198 +137,64 @@ def register_member(request):
 
 @api_view(['POST'])
 def login_member(request):
-    data = request.body
+    data = request.data
+    email = data.get('email')
+    password = data.get('password')
 
-    # Decode the bytes into a string
-    data_str = data.decode('utf-8')
-    
-    #splitlist = str(data).split('&')
-
-    #username_raw = splitlist[0].split('=')[1]
-    #password_raw = splitlist[1].split('=')[1]
-
-    data_dict = json.loads(data_str)
-
-    #parsed_body = QueryDict(data.decode('utf-8'))
-    
-    #print("Parsed body in login_members is ",parsed_body)
-    
-    useremail = data_dict['user_email']
-    password = data_dict['password']
-
-    # print("Username final is ",username)
-    # print("Password final is ",password)
-
-    member = Member.objects.filter(email=useremail).first()
-    tenant = Tenant.objects.filter(email=useremail).first()
-    
-    
-    # if useremail == '' or useremail == None or password == '' or \
-    #     password == None or ((useremail == '' or useremail == None) and \
-    #                          (password == '' or password == None)):
-    #     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    if member == None and tenant == None:
-        try:
-            user_obj = User.objects.get(email=useremail)
-            serializer = UserSerializer(user_obj, many=False)
-            user = serializer.data
-
-            if user['is_superuser']:
-                res = requests.post("https://lobster-app-et3xm.ondigitalocean.app/token/", data=
-                                {
-                                    'username': useremail,
-                                    'password': password
-                                })
-                
-                return Response(data=res.json())
-        except Exception as e:
-            message = {'error': str(e)}
-            return Response(message, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif member != None:
-        member_obj = Member.objects.get(email=useremail)
+    try:
+        user = User.objects.get(email=email)
+        if not user.check_password(password):
+            return Response({"error": "Invalid credentials : user id or password may be incorrect"}, status=status.HTTP_401_UNAUTHORIZED)
         
-        if member_obj.is_verified == False:
-            message = {'error': 'Member is not verified to login to the RWA', 'is_verified': member_obj.is_verified}
-            return Response(message, status=status.HTTP_401_UNAUTHORIZED)
-        
-        token = requests.post("https://lobster-app-et3xm.ondigitalocean.app/token/", data=
-                                {
-                                    'username': useremail,
-                                    'password': password
-                                })
-        res = token.json()
-        res['person_name'] = member_obj.fname
-        res['user_type'] = 'Owner'
-        
-        return Response(data=res)
-    else:
-        tenant_obj = Tenant.objects.get(email=useremail)
-        token = requests.post("https://lobster-app-et3xm.ondigitalocean.app/token/", data=
-                                {
-                                    'username': useremail,
-                                    'password': password
-                                })
-        res = token.json()
-        res['person_name'] = tenant_obj.fname
-        res['user_type'] = 'Tenant'
-        
-        return Response(data=res)
+        if not user.member.is_verified:
+            return Response({"error": "User is not verified"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        return Response({"access_token": access_token}, status=status.HTTP_200_OK)
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def reset_password(request):
-    data = request.body
-
-    # Decode the bytes into a string
-    data_str = data.decode('utf-8')
-
-    data_dict = json.loads(data_str)
-
-    email = data_dict['email']
-    password = data_dict['password']
-    cnfpassword = data_dict['cnfpassword']
-    
-    if password != cnfpassword:
-        message = {'error': 'New password and confirmation password do not match'}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
-    
-    userr = User.objects.get(email=email)   
-    serializer = UserSerializer(userr, many=False)
-    user = serializer.data
-    
-    user['password'] = make_password(password)
-    user.save()
-
-    return Response(status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def send_email_to_client(request):
     try:
-        data = request.body
+        data = request.data
+        email = data.get('email')
+        password = data.get('password')
+        cnfpassword = data.get('cnfpassword')
 
-        # Decode the bytes into a string
-        data_str = data.decode('utf-8')
-
-        data_dict = json.loads(data_str)
-
-        print("Data dict ",data_dict)
+        if password != cnfpassword:
+            return Response({"error": "New password and confirmation password do not match"}, status=status.HTTP_400_BAD_REQUEST)
         
-        subject = "Confirm Email"
-        message = render_to_string('acc_active_email.html', {'nme': data_dict['username']})
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [data_dict['recipient']]
-        send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
+        user = User.objects.get(email=email)
+        user.set_password(password)
+        user.save()
 
-        return Response(status=status.HTTP_200_OK)
-    except Exception as e:
-        message = {'error': str(e)}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "Password reset successfully"}, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def generate_otp(request):
-    data = request.body
+    except User.DoesNotExist:
+        return Response({"error": "User with provided email not found"}, status=status.HTTP_404_NOT_FOUND)
 
-    data_str = data.decode('utf-8')
-
-    payload = json.loads(data_str)
-
-    email = payload.get('email')
-
-    try:
-        member = Member.objects.get(email=email)
-    except Member.DoesNotExist:
-        return Response({"error": "Member not found"}, status=status.HTTP_404_NOT_FOUND)
-    
-    otp = str(random.randint(1000, 9999))
-    member.otp = otp
-    member.save()
-
-    subject = "OTP Verification"
-    message = render_to_string('acc_active_email.html', {'nme': payload['first_name'], 'otp': otp})
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = [email]
-
-    try:
-        send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
-        return Response({"message": "OTP sent successfully"}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['POST'])
-def verify_otp(request):
-    data = request.body
+# @api_view(['POST'])
+# def send_email_to_client(request):
+#     try:
+#         data = request.data
+#         data_str = data.decode('utf-8')
+#         data_dict = json.loads(data_str)
 
-    data_str = data.decode('utf-8')
+#         print("Data dict ",data_dict)
+        
+#         subject = "Confirm Email"
+#         message = render_to_string('acc_active_email.html', {'nme': data_dict['username']})
+#         from_email = settings.EMAIL_HOST_USER
+#         recipient_list = [data_dict['recipient']]
+#         send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
 
-    payload = json.loads(data_str)
-
-    user_otp = payload.get('otp')
-
-    try:
-        refresh = RefreshToken()
-        access_token = refresh.access_token
-
-        expiry_timestamp = access_token.payload['exp']
-        current_timestamp = datetime.utcnow().timestamp()
-    
-        if expiry_timestamp < current_timestamp:
-            return Response({"message": "Refresh token has expired"}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-    except (TokenError, ValueError):
-        return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
-        user = Member.objects.get(otp=user_otp)
-
-        if user.otp == user_otp:
-            user.is_verified = True
-            user.save()
-            return Response({"message": "OTP verified successfully"}, status=status.HTTP_200_OK)
-
-        else:
-            return Response({"error":"OTP formatting"}, status=status.HTTP_400_BAD_REQUEST)
-
-    except Member.DoesNotExist:
-        return Response({"error": "Member not found or Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(status=status.HTTP_200_OK)
+#     except Exception as e:
+#         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
