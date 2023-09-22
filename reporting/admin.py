e54bb7fec8156b4832ab2f8bbd13f41093405c09
ayush-0987 <ayush@vector3.digital>
2023-09-22 13:05:50 +0530

    removed add option from reporting table as it is only for view purpose

--- reporting/admin.py
+++ reporting/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from .models import ProductStock, SaleHistory, PaymentHistory
 # Register your models here.
 
-admin.site.register(ProductStock)
+class ProductStockAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'quantity')
+    def has_add_permission(self, request):
+        return False
+    
+admin.site.register(ProductStock, ProductStockAdmin)
 class SaleHistoryAdmin(admin.ModelAdmin):
     list_display = ('order','package')
+
+    def has_add_permission(self, request):
+        return False
+    
+    def history_view(self, request: HttpRequest, object_id: str, extra_context: None = ...) -> HttpResponse:
+        return super().history_view(request, object_id, extra_context)
+    
 admin.site.register(SaleHistory,SaleHistoryAdmin)
 
 class PaymentHistoryAdmin(admin.ModelAdmin):
     list_display = ('id','customer')
+    def has_add_permission(self,request):
+        return False
 admin.site.register(PaymentHistory, PaymentHistoryAdmin)
