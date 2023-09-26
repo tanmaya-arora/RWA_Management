f6c14e5eaf8365f19da36457ae2e541e79d22ef3
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-09-26 11:59:20 +0530

    Changed verbose name to show Sale History instead of Add Sale History in Sale History section. Commented the add permission which was not showing the add button in Sale History screen

--- reporting/admin.py
+++ reporting/admin.py
@@ -11,9 +11,13 @@ admin.site.register(ProductStock, ProductStockAdmin)
 class SaleHistoryAdmin(admin.ModelAdmin):
     list_display = ('order','package')
 
-    def has_add_permission(self, request):
-        return False  
-admin.site.register(SaleHistory)
+    # def has_add_permission(self, request):
+    #     return False
+    
+    def history_view(self, request: HttpRequest, object_id: str, extra_context: None = ...) -> HttpResponse:
+        return super().history_view(request, object_id, extra_context)
+    
+admin.site.register(SaleHistory,SaleHistoryAdmin)
 
 class PaymentHistoryAdmin(admin.ModelAdmin):
     list_display = ('id','customer')
