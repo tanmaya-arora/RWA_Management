0a1628f1799a8d863e4b8ab461865e1a36948d2e
ayush-0987 <ayush@vector3.digital>
2023-09-26 13:41:09 +0530

    tenant filter, family member filter

--- user_management/admin.py
+++ user_management/admin.py
@@ -9,9 +9,11 @@ class MemberAdmin(admin.ModelAdmin):
 admin.site.register(Owner, MemberAdmin)
 
 class TenantAdmin(admin.ModelAdmin):
-    list_display= ('res_hno','fname','lname')
+    list_display= ('res_hno','fname','lname, is_verified')
+    list_filter = ('gender','is_verified')
 admin.site.register(Tenant, TenantAdmin)
 
 class FamilyMemberAdmin(admin.ModelAdmin):
     list_display = ('fname','relation','gender','family_head')
+    list_filter = ('family_head', 'gender')
 admin.site.register(FamilyMember, FamilyMemberAdmin)
