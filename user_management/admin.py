5cd939314c7f3382148a00b3f20749b653272847
ayush-0987 <ayush@vector3.digital>
2023-09-26 13:51:17 +0530

    pagination to the user management admn pannel

--- user_management/admin.py
+++ user_management/admin.py
@@ -6,14 +6,17 @@ from user_management.models import Owner, Tenant, FamilyMember
 class MemberAdmin(admin.ModelAdmin):
     list_display= ('res_hno','fname','lname','is_verified')
     list_filter = ('gender','is_verified')
+    list_per_page = 5
 admin.site.register(Owner, MemberAdmin)
 
 class TenantAdmin(admin.ModelAdmin):
     list_display= ('res_hno','fname','lname, is_verified')
     list_filter = ('gender','is_verified')
+    list_per_page = 5
 admin.site.register(Tenant, TenantAdmin)
 
 class FamilyMemberAdmin(admin.ModelAdmin):
     list_display = ('fname','relation','gender','family_head')
     list_filter = ('family_head', 'gender')
+    list_per_page = 5
 admin.site.register(FamilyMember, FamilyMemberAdmin)
