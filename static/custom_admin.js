fd5ccc93b0fe97aac5fd64886e3bbe785533ae2a
ayush-0987 <ayush@vector3.digital>
2023-10-05 10:22:12 +0530

    update to the search bar of the ticket field

--- static/custom_admin.js
+++ static/custom_admin.js
@@ -1,3 +1,4 @@
 window.onload = function() {
-    document.getElementById("searchbar").placeholder = "Search by name or ticket id ";
+    document.getElementById("searchbar").placeholder = "@Search by name or ticket id ";
+    document.getElementById("searchbar").style.fontStyle= "italic";
 };
