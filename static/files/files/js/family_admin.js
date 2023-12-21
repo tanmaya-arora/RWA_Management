4116cde89f0bb161df203a1a82c0cbcd4b94c01a
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-12-21 14:58:42 +0530

    Django volt theme - collected static files

---
+++ static/files/files/js/family_admin.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.getElementById("searchbar").placeholder = "Search by family head or name";
+    document.getElementById("searchbar").style.fontStyle= "italic";
+};
---
+++ static/files/files/js/owner_admin.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.getElementById("searchbar").placeholder = "Search by Name or H.no.";
+    document.getElementById("searchbar").style.fontStyle= "italic";
+}
